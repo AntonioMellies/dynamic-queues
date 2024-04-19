a98ddb4e12ee11c90099c27d35e910fabe0e56b1
Antonio Mellies <neto-fred@live.com>
2024-04-19 01:35:57 -0300

    rabbitmq consumer adjustments

--- dynamic-queue-consumer/src/main/java/br/com/mellies/antonio/consumer/infrastracture/converter/QueueMessageKafkaDeserializer.java
+++ dynamic-queue-consumer/src/main/java/br/com/mellies/antonio/consumer/infrastracture/converter/QueueMessageKafkaDeserializer.java
@@ -3,8 +3,8 @@ package br.com.mellies.antonio.consumer.infrastracture.converter;
 import br.com.mellies.antonio.core.queue.QueueMessage;
 import io.quarkus.kafka.client.serialization.ObjectMapperDeserializer;
 
-public class QueueMessageDeserializer extends  ObjectMapperDeserializer<QueueMessage> {
-  public QueueMessageDeserializer() {
+public class QueueMessageKafkaDeserializer extends ObjectMapperDeserializer<QueueMessage> {
+  public QueueMessageKafkaDeserializer() {
     super(QueueMessage.class);
   }
 }
--- dynamic-queue-consumer/src/main/java/br/com/mellies/antonio/consumer/infrastracture/queue/ReceiveSimpleMessageRabbitmqConsumer.java
+++ dynamic-queue-consumer/src/main/java/br/com/mellies/antonio/consumer/infrastracture/queue/ReceiveSimpleMessageRabbitmqConsumer.java
@@ -8,6 +8,7 @@ import br.com.mellies.antonio.core.queue.QueueMessage;
 import br.com.mellies.antonio.core.queue.QueueReceiver;
 import io.quarkus.arc.properties.IfBuildProperty;
 import io.quarkus.logging.Log;
+import io.vertx.core.json.JsonObject;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import java.util.concurrent.CompletionStage;
@@ -18,26 +19,33 @@ import org.eclipse.microprofile.reactive.messaging.Message;
 @IfBuildProperty(name = "queue.system", stringValue = "rabbitmq")
 public class ReceiveSimpleMessageRabbitmqConsumer
     implements ReceiveSimpleMessagePort<QueueMessage<String>>,
-        QueueReceiver<CompletionStage<Void>, Message<QueueMessage<String>>> {
+        QueueReceiver<CompletionStage<Void>, Message<JsonObject>> {
 
-  @Inject
-  ReceiveSimpleMessageUseCase receiveSimpleMessageUseCase;
+  @Inject ReceiveSimpleMessageUseCase receiveSimpleMessageUseCase;
 
-  @Override
   @Incoming("channel-test")
-  public CompletionStage<Void> receive(Message<QueueMessage<String>> message) {
+  public CompletionStage<Void> receive(Message<JsonObject> message) {
     try {
       Log.info("Received a message by Rabbitmq");
-      this.receiveMessage(message.getPayload());
+      this.receiveMessage(jsonObjectToQueueMessage(message.getPayload()));
     } catch (Exception e) {
+      Log.error(e);
       return message.nack(e);
     }
     return message.ack();
   }
-  @Override
-  public void receiveMessage(QueueMessage<String> body) throws CustomException {
-    var input = new ReceiveSimpleMessageUseCaseInput(body.getId().toString(), body.getData());
+
+  public void receiveMessage(QueueMessage<String> message) throws CustomException {
+    var input = new ReceiveSimpleMessageUseCaseInput(message.getId().toString(), message.getData());
     receiveSimpleMessageUseCase.execute(input);
   }
 
+  private QueueMessage<String> jsonObjectToQueueMessage(JsonObject jsonObject)
+      throws CustomException {
+    try {
+      return jsonObject.mapTo(QueueMessage.class);
+    } catch (Exception e) {
+      throw new CustomException("Error converting received message");
+    }
+  }
 }
